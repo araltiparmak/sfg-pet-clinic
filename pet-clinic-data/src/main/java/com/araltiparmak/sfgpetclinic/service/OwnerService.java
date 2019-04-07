9b88a28597a8989e4e3050465de53add2758914d
araltiparmak <aliriza_altiparmak@hotmail.com>
2019-04-07 09:58:06 +0300

    refaactored to common crud interfacea

--- pet-clinic-data/src/main/java/com/araltiparmak/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/araltiparmak/sfgpetclinic/service/OwnerService.java
@@ -2,25 +2,9 @@ package com.araltiparmak.sfgpetclinic.service;
 
 import com.araltiparmak.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
-    default String a() {
-        return "a";
-    }
-
-    static String b() {
-        return "b";
-    }
 
 
 }
--- pet-clinic-data/src/main/java/com/araltiparmak/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/araltiparmak/sfgpetclinic/service/VetService.java
@@ -2,13 +2,6 @@ package com.araltiparmak.sfgpetclinic.service;
 
 import com.araltiparmak.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
