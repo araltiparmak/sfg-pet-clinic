59d0a747d87aa392664ae43f28479d2aa9ad08af
araltiparmak <aliriza_altiparmak@hotmail.com>
2019-04-07 10:23:50 +0300

    added map based services

---
+++ pet-clinic-data/src/main/java/com/araltiparmak/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.araltiparmak.sfgpetclinic.service.map;
+
+import com.araltiparmak.sfgpetclinic.model.Vet;
+import com.araltiparmak.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+}
